14768ace86409fc7a916b883bf3ac3f40443756f
LeoHeo <hjh5488@gmail.com>
2016-06-21 16:23:02 +0800

    add flash messages: add logout flash message

--- fastube/users/views/logout.py
+++ fastube/users/views/logout.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.views.generic.base import RedirectView
 from django.core.urlresolvers import reverse
 from django.contrib.auth import logout
@@ -7,4 +8,9 @@ class LogoutView(RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
         logout(self.request)
+        messages.add_message(
+            self.request,
+            messages.SUCCESS,
+            "성공적으로 로그아웃 되었습니다.",
+        )
         return reverse("home")
