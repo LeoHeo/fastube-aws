ef37562341b788c752d20d5caceaad3469e8e090
LeoHeo <hjh5488@gmail.com>
2016-06-30 02:32:04 +0800

    delete pyenv command

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -6,7 +6,6 @@ chown ec2-user:ec2-user /home/ec2-user/www
 virtualenv --python=python3.4 /home/ec2-user/www/fastube-venv
 
 chown ec2-user:ec2-user /home/ec2-user/www/fastube-venv
-pyenv activate fastube
 
 source /home/ec2-user/www/fastube-venv/bin/activate
 
