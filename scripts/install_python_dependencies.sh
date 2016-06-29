62bd1170d2875dc891022d6288ff368d4ae6517a
LeoHeo <hjh5488@gmail.com>
2016-06-30 02:04:41 +0800

    refactor python depencies runas test

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -7,6 +7,6 @@ virtualenv --python=python3.4 /home/ec2-user/www/fastube-venv
 
 chown ec2-user:ec2-user /home/ec2-user/www/fastube-venv
 
-source /home/ec2-user/www/fastube/bin/activate
+source /home/ec2-user/www/fastube-venv/bin/activate
 
 pip install -r /home/ec2-user/www/fastube/requirements.txt
